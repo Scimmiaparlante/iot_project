b20defbf3e74fbf2dfffed3674a7d4eac0be4316
Matteo Zini <matteozini96@gmail.com>
2020-05-28 17:55:47 +0200

    Added temp pressure with triggers from the button.

---
+++ nodes/temperature_sensor/main.c
@@ -0,0 +1,148 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "contiki.h"
+#include "coap-engine.h"
+#include "coap-blocking-api.h"
+#include "coap-constants.h"
+#include "os/dev/button-hal.h"
+
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "TEMP_SENSOR"
+#define LOG_LEVEL LOG_LEVEL_INFO
+
+#define REG_SEND_INTERVAL 		(3*CLOCK_SECOND)
+
+#define PUBLISH_INTERVAL 		(20*CLOCK_SECOND)
+
+// Server IP and resource path
+#define SERVER_EP "coap://[fd00::1]:5683"
+
+//---------------- I/O DEVICES -------------------------
+button_hal_button_t* btn; 
+//------------------------------------------------------
+
+
+//---------------- RESOURCES LIST ----------------------
+extern coap_resource_t temperature_res;
+//------------------------------------------------------
+
+
+/* Declare and auto-start the server and registrator processses */
+PROCESS(temp_registration_process, "Registration process for temperature");
+PROCESS(server_process, "Server process");
+AUTOSTART_PROCESSES(&temp_registration_process, &server_process);
+
+
+/* ---------------------------------------------------------------------------
+ * ----------------------- REGISTRATION PROCESSES ----------------------------
+ * -------------------------------------------------------------------------*/
+
+//global variable to stop the main loop when the registration is completed
+uint8_t registered_temperature = 0;
+
+
+// Define a handler to handle the response from the server
+void response_handler_temperature(coap_message_t* response) 
+{
+	LOG_INFO("Temperature sensor - Handling registration response...\n");
+	
+	if(response == NULL) {
+		LOG_INFO("Temperature sensor - Request timed out\n");
+		return;
+	}
+
+	if(response->code == CHANGED_2_04) {
+		LOG_INFO("Temperature sensor - Registration successful\n");
+		registered_temperature = 1;
+	} else if (response->code == BAD_REQUEST_4_00) {
+		LOG_INFO("Temperature sensor - Registration failed: BAD REQUEST\n");
+	} else {
+		LOG_INFO("Temperature sensor - Registration failed: unknown error: %x\n", response->code);
+	}
+}
+
+
+PROCESS_THREAD(temp_registration_process, ev, data)
+{
+	static coap_endpoint_t server_ep;
+	static coap_message_t request[1];
+	static struct etimer periodic_timer;
+	char resource_path[] = "/remote_dir";
+	
+  	PROCESS_BEGIN();
+
+	//timer to send the request again in case of failure
+	etimer_set(&periodic_timer, REG_SEND_INTERVAL);
+
+	// Populate the coap_endpoint_t data structure
+	coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
+
+  	LOG_INFO("Contiki-NG coap client started\n");
+
+	while(!registered_temperature) {
+   		PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER && data == &periodic_timer);
+
+	  	// Prepare the message
+	  	coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
+	  	coap_set_header_uri_path(request, resource_path);
+	  	
+	  	// Set the payload
+	  	const char msg[] = "{\"a\" : \"register\" , \"t\" : \"sensor\" , \"st\" : \"temperature\" , \"p\" : \"/temperature\"}";
+	  	coap_set_payload(request, (uint8_t *)msg, sizeof(msg) -1);
+	  	coap_set_header_content_format(request, APPLICATION_JSON);
+	  	
+	  	// Issue the request in a blocking manner 
+	  	// The function returns when the request has been sent (ack received)
+	  	LOG_INFO("Temperature sensor - Issuing registration request...\n");
+	  	COAP_BLOCKING_REQUEST(&server_ep, request, response_handler_temperature);
+	  	
+	  	etimer_reset(&periodic_timer);
+	}
+
+  	PROCESS_END();
+}
+
+
+
+/* ---------------------------------------------------------------------------
+ * --------------------------- SERVER PROCESS --------------------------------
+ * -------------------------------------------------------------------------*/
+ 
+uint8_t n_button_press = 0;
+ 
+PROCESS_THREAD(server_process, ev, data) 
+{
+	static struct etimer periodic_timer;
+	
+  	PROCESS_BEGIN();
+
+	//timer to send the request again in case of failure
+	etimer_set(&periodic_timer, PUBLISH_INTERVAL);
+	
+	clock_init();
+	srand(clock_time());
+	
+	button_hal_init();
+	btn = button_hal_get_by_index(0);
+	
+	// Activation of a resource
+	coap_activate_resource(&temperature_res, "temperature");	
+
+  	LOG_INFO("Coap server started!\n");
+
+	while(1) {
+   		PROCESS_WAIT_EVENT_UNTIL((ev == PROCESS_EVENT_TIMER && data == &periodic_timer) || ev == button_hal_press_event);
+
+		if(ev == button_hal_press_event)
+			n_button_press++;
+		else
+		  	temperature_res.trigger();	  	
+		  	
+		etimer_reset(&periodic_timer);
+	}
+
+  	PROCESS_END();
+}
+
+
---
+++ nodes/temperature_sensor/resources/temperature_res.c
@@ -0,0 +1,94 @@
+#include <stdlib.h>
+#include <string.h>
+#include "coap-engine.h"
+#include "coap-constants.h"
+#include "os/dev/button-hal.h"
+
+#include "sys/log.h"
+#define LOG_MODULE "TEMP_RES"
+#define LOG_LEVEL LOG_LEVEL_INFO
+
+
+static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void res_event_handler(void);
+
+
+EVENT_RESOURCE(temperature_res,
+         "title=\"temperature\";rt=\"Text\"",
+         res_get_handler,
+         NULL,
+         NULL,
+         NULL, 
+		 res_event_handler);
+
+
+//------------------------------ I/O EMULATION ----------------------------------------------------
+
+extern button_hal_button_t* btn;
+extern uint8_t n_button_press;
+
+enum {NORMAL, HOT, COLD} temp_state = NORMAL;
+
+static float readTemperature() 
+{
+	static float old_temp = 20;
+	float ret;
+
+
+	//if button pressed, cycle through the states
+	if (n_button_press > 0)		
+		temp_state = (temp_state + n_button_press) % 3;
+		
+	n_button_press = 0;
+		
+	if (temp_state == NORMAL) {
+		ret = ( old_temp + ((rand() % 2 == 0) ? -1 : 1) * ( ((float)(rand() % 100)) / 500 ) );
+		old_temp = ret;
+	}
+	else if (temp_state == HOT)
+		ret = 50;
+	else
+		ret = 0;
+	
+	return ret;
+}
+
+//-------------------------- END I/O EMULATION -------------------------------------------------------
+
+
+static float temp;
+
+static void res_event_handler(void)
+{
+	//DO THINGS
+	temp = readTemperature();
+	
+	LOG_INFO("Notifying everyone\n");
+	
+    // Notify all the observers
+    coap_notify_observers(&temperature_res);
+}
+
+
+static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	int len;
+	
+	LOG_INFO("Handling get request\n");
+	
+	coap_set_header_content_format(response, APPLICATION_JSON);
+	
+	
+	len = snprintf((char *)buffer, preferred_size, 
+	"{\"bt\": \"%lu\", \"bn\" : \"temperature\", \"bu\" : \"° C\",\"e\" : [ {\"v\" : \"%d\"} ]}", 
+																	clock_seconds(), (int) temp);
+	len = (preferred_size < len) ? preferred_size : len;
+  
+  	coap_set_payload(response, buffer, len);
+}
+
+
+
+
+
+
