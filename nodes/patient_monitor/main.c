94a9ebefd487a09efdbb9edba91720efb37c65ee
Matteo Zini <matteozini96@gmail.com>
2020-05-28 12:25:04 +0200

    Removed alarm and temperature from patient_monitor

--- nodes/patient_monitor/main.c
+++ nodes/patient_monitor/main.c
@@ -10,7 +10,7 @@
 #define LOG_MODULE "PAT_MONITOR"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-#define REG_SEND_INTERVAL 			(3*CLOCK_SECOND)
+#define REG_SEND_INTERVAL 		(3*CLOCK_SECOND)
 
 #define PUBLISH_INTERVAL 		(3*CLOCK_SECOND)
 
@@ -20,7 +20,6 @@
 
 //---------------- RESOURCES LIST ----------------------
 extern coap_resource_t heartbeat_res;
-extern coap_resource_t alarm_res;
 extern coap_resource_t bloodpressure_res;
 //------------------------------------------------------
 
@@ -28,9 +27,8 @@ extern coap_resource_t bloodpressure_res;
 /* Declare and auto-start the server and registrator processses */
 PROCESS(heartbeat_registration_process, "Registration process for heartbeat");
 PROCESS(bloodpressure_registration_process, "Registration process for blood pressure");
-PROCESS(alarm_registration_process, "Registration process for alarm");
 PROCESS(server_process, "Server process");
-AUTOSTART_PROCESSES(&heartbeat_registration_process, &server_process, &alarm_registration_process, &bloodpressure_registration_process);
+AUTOSTART_PROCESSES(&heartbeat_registration_process, &server_process, &bloodpressure_registration_process);
 
 
 /* ---------------------------------------------------------------------------
@@ -169,74 +167,6 @@ PROCESS_THREAD(bloodpressure_registration_process, ev, data)
 }
 
 
-//---------------------------------------------------------------------------------------------------
-
-//global variable to stop the main loop when the registration is completed
-uint8_t registered_alarm = 0;
-
-
-// Define a handler to handle the response from the server
-void response_handler_alarm(coap_message_t* response) 
-{
-	LOG_INFO("Alarm - Handling registration response...\n");
-	
-	if(response == NULL) {
-		LOG_INFO("Alarm - Request timed out\n");
-		return;
-	}
-
-	if(response->code == CHANGED_2_04) {
-		LOG_INFO("Alarm - Registration successful\n");
-		registered_alarm = 1;
-	} else if (response->code == BAD_REQUEST_4_00) {
-		LOG_INFO("Alarm - Registration failed: BAD REQUEST\n");
-	} else {
-		LOG_INFO("Alarm - Registration failed: unknown error: %x\n", response->code);
-	}
-}
-
-
-PROCESS_THREAD(alarm_registration_process, ev, data)
-{
-	static coap_endpoint_t server_ep;
-	static coap_message_t request[1];
-	static struct etimer periodic_timer;
-	char resource_path[] = "/remote_dir";
-	
-  	PROCESS_BEGIN();
-
-	//timer to send the request again in case of failure
-	etimer_set(&periodic_timer, REG_SEND_INTERVAL);
-
-	// Populate the coap_endpoint_t data structure
-	coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
-
-  	LOG_INFO("Contiki-NG coap client started\n");
-
-	while(!registered_alarm) {
-   		PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER && data == &periodic_timer);
-
-	  	// Prepare the message
-	  	coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
-	  	coap_set_header_uri_path(request, resource_path);
-	  	
-	  	// Set the payload
-	  	const char msg[] = "{\"a\" : \"register\" , \"t\" : \"actuator\" , \"st\" : \"alarm\" , \"p\" : \"/alarm\"}";
-	  	coap_set_payload(request, (uint8_t *)msg, sizeof(msg) -1);
-	  	coap_set_header_content_format(request, APPLICATION_JSON);
-	  	
-	  	// Issue the request in a blocking manner 
-	  	// The function returns when the request has been sent (ack received)
-	  	LOG_INFO("Alarm - Issuing registration request...\n");
-	  	COAP_BLOCKING_REQUEST(&server_ep, request, response_handler_alarm);
-	  	
-	  	etimer_reset(&periodic_timer);
-	}
-
-  	PROCESS_END();
-}
-
-
 
 /* ---------------------------------------------------------------------------
  * --------------------------- SERVER PROCESS --------------------------------
@@ -257,7 +187,6 @@ PROCESS_THREAD(server_process, ev, data)
 	// Activation of a resource
 	coap_activate_resource(&heartbeat_res, "heartbeat");	
 	coap_activate_resource(&bloodpressure_res, "bloodpressure");
-	coap_activate_resource(&alarm_res, "bloodpressure");
 
   	LOG_INFO("Coap server started!\n");
 
