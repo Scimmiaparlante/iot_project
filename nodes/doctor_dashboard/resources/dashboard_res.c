6a1569252a9338c332bdcd698420ab7a1659ed81
Matteo Zini <matteozini96@gmail.com>
2020-05-29 18:31:39 +0200

    Dashboard adapted to show message origin

--- nodes/doctor_dashboard/resources/dashboard_res.c
+++ nodes/doctor_dashboard/resources/dashboard_res.c
@@ -9,6 +9,7 @@
 #define LOG_LEVEL LOG_LEVEL_INFO
 
 #define BUFFER_LEN 				200
+#define ADDR_BUFFER_LEN			50
 #define MIN_INTERMESSAGE_TIME	20
 
 
@@ -32,16 +33,18 @@ static void res_post_handler(coap_message_t *request, coap_message_t *response, 
 	char message[BUFFER_LEN];
 	size_t len = 0;
 	const char* msg_ptr;
+	char origin[ADDR_BUFFER_LEN];			//ip address of the node originating the message
 
 	//LOG_INFO("Handling post request\n");
 	
 	coap_set_header_content_format(response, TEXT_PLAIN);
 	response->code = CHANGED_2_04;
 	
-	memset(message, 0, BUFFER_LEN);	
+	memset(message, 0, BUFFER_LEN);
+	memset(origin, 0, ADDR_BUFFER_LEN);	
 	
+	//retrieve message
 	len = coap_get_post_variable(request, "msg", &msg_ptr);
-	
 	if (len >= BUFFER_LEN) {
 		response->code = BAD_REQUEST_4_00;
 		return;
@@ -49,10 +52,19 @@ static void res_post_handler(coap_message_t *request, coap_message_t *response, 
 	
 	memcpy(message, msg_ptr, len);
 	
+	//retrieve source ip
+	len = coap_get_post_variable(request, "or", &msg_ptr);
+	if (len >= ADDR_BUFFER_LEN) {
+		response->code = BAD_REQUEST_4_00;
+		return;
+	}
+	
+	memcpy(origin, msg_ptr, len);
+	
 	//I don't display the same message twice consecutively. Wait MIN_INTERMESSAGE_TIME before showing again.
 	//However, If two different messages are sent concurrently, I'll end up showing both muliple times.
 	if(strcmp(message, old_message) != 0 || (clock_seconds() - last_message_time) > MIN_INTERMESSAGE_TIME) {
-		LOG_INFO("------- DASHBOARD:  = %s\n", message);
+		LOG_INFO("DASHBOARD: %s - source: %s\n", message, origin);
 		
 		last_message_time = clock_seconds();
 		strcpy(old_message, message);
