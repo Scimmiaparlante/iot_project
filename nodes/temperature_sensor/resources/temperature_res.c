ab13d40ec22556f1ffc1f465d04ac53814716586
Matteo Zini <matteozini96@gmail.com>
2020-05-30 00:45:30 +0200

    The temp sensor can receive the state ot the air conditioning system

--- nodes/temperature_sensor/resources/temperature_res.c
+++ nodes/temperature_sensor/resources/temperature_res.c
@@ -8,15 +8,18 @@
 #define LOG_MODULE "TEMP_RES"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
+#define RAND_SIGN 	(rand() % 2 == 0) ? -1 : 1)
+
 
 static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void simulation_res_post_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 static void res_event_handler(void);
 
 
 EVENT_RESOURCE(temperature_res,
          "title=\"temperature\";rt=\"Text\"",
          res_get_handler,
-         NULL,
+         simulation_res_post_handler,	//this resource is used to emulate the effect of the air-cond system on temperature
          NULL,
          NULL, 
 		 res_event_handler);
@@ -27,29 +30,38 @@ EVENT_RESOURCE(temperature_res,
 extern button_hal_button_t* btn;
 extern uint8_t n_button_press;
 
-enum {NORMAL, HOT, COLD} temp_state = NORMAL;
+enum {NORMAL, HOT, COLD} temp_force_state = NORMAL;
+
+//aircond effect simulation
+uint8_t aircond_state = 0;
 
 static float readTemperature() 
 {
-	static float old_temp = 20;
-	float ret;
+	static float temp = 20;
 
 	//if button pressed, cycle through the states
 	if (n_button_press > 0)		
-		temp_state = (temp_state + n_button_press) % 3;
+		temp_force_state = (temp_force_state + n_button_press) % 3;
 		
+	if (temp_force_state == HOT)
+		temp = 50;
+	else if (temp_force_state == COLD)
+		temp = 0;
+		
+	//the effect of the button push has been propagated, reset to normal
+	temp_force_state = NORMAL;
 	n_button_press = 0;
 		
-	if (temp_state == NORMAL) {
-		ret = ( old_temp + ((rand() % 2 == 0) ? -1 : 1) * ( ((float)(rand() % 100)) / 500 ) );
-		old_temp = ret;
-	}
-	else if (temp_state == HOT)
-		ret = 50;
-	else
-		ret = 0;
+	temp = ( temp + (RAND_SIGN * ( ((float)(rand() % 10)) / 10 ) );
 	
-	return ret;
+	//--- simulate the effect of the air conditioning system (+/- 10 degrees <==> 0.5 °C/second)
+	if (aircond_state == 2)
+		temp -= 10;
+	else if (aircond_state == 1)
+		temp += 10;
+	//-----------------------------
+		
+	return temp;
 }
 
 //-------------------------- END I/O EMULATION -------------------------------------------------------
@@ -87,7 +99,30 @@ static void res_get_handler(coap_message_t *request, coap_message_t *response, u
 }
 
 
+static void simulation_res_post_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	size_t len = 0;
+	const char* msg_ptr;
 
+	LOG_INFO("Handling post request - fake temperature effect\n");
+	
+	coap_set_header_content_format(response, TEXT_PLAIN);
+	response->code = BAD_REQUEST_4_00;
+	
+	//retrieve message
+	len = coap_get_post_variable(request, "cmd", &msg_ptr);
+	if(len == 1) {
+		if (*msg_ptr == '2')			//COLD
+			aircond_state = 2;
+		else if (*msg_ptr == '1')		//HOT
+			aircond_state = 1;
+		else if (*msg_ptr == '0')		//OFF
+			aircond_state = 0;
 
+		response->code = CHANGED_2_04;
+	}
+	
+	LOG_INFO("The temp sensor got to know the air-cond system is in state %u\n", aircond_state);
+}
 
 
